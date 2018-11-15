6601ad8eb090b4a1c633d021bae6f7aa9a8028b5
Karim Harat <karim@marmelapp.com>
2018-11-15 16:28:41 +0100

    Added index.d.ts, put throttlebutton class in its own file

---
+++ ThrottleButton.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react"
+import {TouchableOpacity} from "react-native"
+
+export default class ThrottleButton extends Component {
+
+    canPress = true;
+
+    _onPress = () => {
+        if (this.canPress) {
+            this.props.onPress();
+            this.canPress = false;
+            setTimeout(() => this.canPress = true, this.props.pressThrottle);
+        }
+    };
+
+    render() {
+        return (
+            <TouchableOpacity {...this.props} onPress={this._onPress}>
+                {this.props.children}
+            </TouchableOpacity>
+        );
+    }
+}
+
+ThrottleButton.defaultProps = {
+    pressThrottle: 300,
+    onPress: () => true
+};

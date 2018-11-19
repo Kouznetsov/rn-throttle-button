f76da3db7638f5fd41372d55791893b47c7a3bf3
Karim Harat <karim@marmelapp.com>
2018-11-19 11:06:56 +0100

    I have no idea what i'm doing

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,7 @@ import React, {Component} from "react";
 import {TouchableOpacityProps} from "react-native";
 
 declare module "rn-throttle-button" {
-
-    export interface ThrottleButtonProps extends TouchableOpacityProps {
-        children?: Component
-    }
-
-    export class ThrottleButton extends Component<ThrottleButtonProps> {}
-
+    export class ThrottleButton extends Component<TouchableOpacityProps> {}
 }
 
 export default ThrottleButton;

04d46b726c94c7ead2ae7c9d81fb6c6e66da6d25
Karim Harat <karim@marmelapp.com>
2018-11-19 10:42:17 +0100

    Updated index.d.ts to expose props, updated npm version to 1.0.0

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,14 @@
-import ThrottleButton from "./ThrottleButton"
+import React, {Component} from "react";
+import {TouchableOpacityProps} from "react-native";
+
+declare module "rn-throttle-button" {
+
+    export interface ThrottleButtonProps extends TouchableOpacityProps {
+        children?: Component
+    }
+
+    export class ThrottleButton extends Component<ThrottleButtonProps> {}
+
+}
 
 export default ThrottleButton;

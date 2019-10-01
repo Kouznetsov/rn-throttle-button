1488ce981f08e59ab7d0bf5c60e308fbac5c9ae8
AlanKeiss <alan.keiss@gmail.com>
2019-10-01 16:32:06 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import {TouchableOpacityProps} from "react-native";
 
 interface Props extends TouchableOpacityProps{
-    pressThrottle: number;
+    pressThrottle?: number;
 }
 
 declare module "rn-throttle-button" {
